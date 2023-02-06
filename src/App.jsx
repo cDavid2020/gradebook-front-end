1ffd2209d23844ecd98ad9d3d39c1f72b721ad2f
Manav Misra <manavm@visionwebsoft.com>
2023-02-06 13:06:13 -0600

    Refactor into more dynamic input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import InputText from "./components/form/input-text";
-import InputPassword from "./components/form/input-password";
 
 function App() {
   return (
     <form className="mt-4 flex flex-col items-center gap-y-2">
-      <InputText />
+      <InputText label="Username" id="username" />
 
-      <InputPassword />
-      <InputPassword />
+      <InputText label="Password" id="password" type="password" />
+      <InputText
+        label="Confirm Password"
+        id="confirmPassword"
+        type="password"
+      />
 
       {/* TODO: Toggle between Register and Login */}
       <button
--- src/components/form/input-text.jsx
+++ src/components/form/input-text.jsx
@@ -1,16 +1,28 @@
-export default function InputText() {
+import PropTypes from "prop-types";
+
+export default function InputText({ label, id, type }) {
   return (
     <>
-      <label htmlFor="username" className="sr-only">
-        Username
+      <label htmlFor={id} className="sr-only">
+        {label}
       </label>
       <input
-        type="text"
-        id="username"
-        name="username"
-        placeholder="Username"
+        type={type}
+        id={id}
+        name={id}
+        placeholder={label}
         className="rounded"
       />
     </>
   );
 }
+
+InputText.defaultProps = {
+  type: "text",
+};
+
+InputText.propTypes = {
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
